7a872245f9fc526b099ae8b8ceadf31880e203f9
Gabriel Hidasy <gabrielhidasy@gmail.com>
2015-10-24 13:20:46 -0200

    Feito, mas o caso de testes fornecido da invalido
    Falta agora só extrair os valores do lemon e passar pra main, mas deve
    ser facil, o gurobi já reporta direitinho

--- lab4/transmissao_dados/transmissao_dados.cpp
+++ lab4/transmissao_dados/transmissao_dados.cpp
@@ -91,7 +91,7 @@ bool transmissoes(Problem_Data &G, long maxtime)
   model.set(GRB_IntAttr_ModelSense, GRB_MINIMIZE);
   // is a minimization problem
   G.NPairs = G.s.size(); //Correcting strange problem in input
-  cout << "NPairs " << G.NPairs << endl;
+  //cout << "NPairs " << G.NPairs << endl;
 
   //Two variables for each edge for each pair
   ListGraph::EdgeMap< vector <GRBVar> > g(G.g);
@@ -104,18 +104,21 @@ bool transmissoes(Problem_Data &G, long maxtime)
   //   (b[i])(G.g);
   // }
 
-  char namme[100];
-  int i = 0;
+  char nammeG[100];
+  char nammeB[100];
   double tmp = 0;
   //Funcao de minimizacao
   for (ListGraph::EdgeIt e(G.g); e != INVALID; ++e) {
-    sprintf(namme,"E_%d_%lf",i++,tmp);
+    //cout << G.g.id(G.g.u(e)) << "-->" << G.g.id(G.g.v(e)) << endl;
     tmp = G.custo[e];
     g[e].resize(G.NPairs);
     b[e].resize(G.NPairs);
     for (int z = 0; z < G.NPairs; z++) {
-      g[e][z] = model.addVar(0,1,tmp*G.q[z],GRB_BINARY,namme);
-      b[e][z] = model.addVar(0,1,tmp*G.q[z],GRB_BINARY,namme);
+      sprintf(nammeG,"EG_%d_%d_%d",G.g.id(G.g.u(e))+1,G.g.id(G.g.v(e))+1,z);
+      sprintf(nammeB,"EB_%d_%d_%d",G.g.id(G.g.v(e))+1,G.g.id(G.g.u(e))+1,z);
+      //cout << tmp <<  " " << G.q[z] << endl;
+      g[e][z] = model.addVar(0,1,tmp*G.q[z],GRB_BINARY,nammeG);
+      b[e][z] = model.addVar(0,1,tmp*G.q[z],GRB_BINARY,nammeB);
     }
   }
   model.update();
@@ -123,10 +126,12 @@ bool transmissoes(Problem_Data &G, long maxtime)
   for (int z = 0; z < G.NPairs; z++) {
     GRBLinExpr expr;
     for (ListGraph::EdgeIt e(G.g); e != INVALID; ++e) {
-      expr += g[e][z];
-      expr += b[e][z];
+      expr += g[e][z]*G.latencia[e];
+      expr += b[e][z]*G.latencia[e];
+      //cout << G.g.id(G.g.u(e)) << " " << G.g.id(G.g.v(e)) << " " << G.latencia[e] << endl;
     }
     model.addConstr(expr <= G.Tmax[z]);
+    //cout << G.Tmax[z] << endl;
   }
   model.update();
   // Restricao de capacidade
@@ -144,23 +149,47 @@ bool transmissoes(Problem_Data &G, long maxtime)
     for (ListGraph::NodeIt v(G.g); v != INVALID; ++v) {
       //Para cada aresta saindo de v
       GRBLinExpr expr;
-      for (ListGraph:IncEdgeIt e(G.g, v); e != INVALID; ++e) {
+      for (ListGraph::IncEdgeIt e(G.g, v); e != INVALID; ++e) {
 	//Para cada aresta desse no
-	expr += g[e][z];
-	expr -= b[e][z];
+	if (G.g.u(e) == v) {
+	  expr += g[e][z];
+	  expr -= b[e][z];
+	} else {
+	  expr -= g[e][z];
+	  expr += b[e][z];
+	}
       }
-      //se v for membro de s
-      //model.addConstr(expr == 1);
-      //se v for membro de t
-      //model.addConstr(expr == -1);
-      //else
-      //model.addConstr(expr == 0);
+      //cout << G.g.id(v) << endl;
+      if (G.g.id(v) == G.g.id(G.s[z])) {
+	//se v for membro de s
+	model.addConstr(expr == 1);
+      } else {
+	if (G.g.id(v) == G.g.id(G.t[z])) {
+	  //se v for membro de t
+	  model.addConstr(expr == -1);
+	} else {
+	  model.addConstr(expr == 0);
+	}
+      }
     }
   }
   model.update();
   try {
-    //model.optimize();
-    model.write("model.lp"); system("cat model.lp");
+    model.optimize();
+    //model.write("model.lp"); system("cat model.lp");
+    if (model.get(GRB_IntAttr_Status) != GRB_OPTIMAL) {
+      cout << "Erro, sistema impossivel" << endl;
+      exit(1);
+    }
+    // for (ListGraph::EdgeIt e(G.g); e != INVALID; ++e) {
+    //     for (int z = 0; z < G.NPairs; z++) {
+    // 	if (g[e][z].get(GRB_Double_Attr_X) > 0.9)
+    // 	g[e][z] = model.addVar(0,1,tmp*G.q[z],GRB_BINARY,nammeG);
+    // 	b[e][z] = model.addVar(0,1,tmp*G.q[z],GRB_BINARY,nammeB);
+    //   }
+    // }
+    
+    
     return 1;
   } catch (...)
     {
